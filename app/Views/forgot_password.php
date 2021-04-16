ab8e07ad0a5056a3f3235b8aeb0f2e2b5f3709ad
Renan Brito <renan.brito@qualidoc.com.br>
2021-04-16 09:32:52 -0300

    COmmit feito

--- app/Views/forgot_password.php
+++ app/Views/forgot_password.php
@@ -2,21 +2,7 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title>Esqueci a senha</title>
-
-    <!-- Custom fonts for this template-->
-    <?php echo link_tag('vendor/fontawesome-free/css/all.min.css');?>
-    <?php echo link_tag('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');?>
-
-    <!-- Custom styles for this template-->
-    <?php echo link_tag('css/sb-admin-2.min.css');?>
 </head>
 
 <body class="bg-gradient-primary">
@@ -68,17 +54,5 @@
         </div>
 
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <?php echo script_tag('vendor/jquery/jquery.min.js'); ?>
-    <?php echo script_tag('vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>
-
-    <!-- Core plugin JavaScript-->
-    <?php echo script_tag('vendor/jquery-easing/jquery.easing.min.js'); ?>
-
-    <!-- Custom scripts for all pages-->
-    <?php echo script_tag('js/sb-admin-2.min.js'); ?>
-
 </body>
-
 </html>
