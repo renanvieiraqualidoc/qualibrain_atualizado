56a52a7a99b23dd1217b9b3b32670b8387f3b7ed
Renan Brito <renan.brito@qualidoc.com.br>
2021-04-20 11:08:00 -0300

    Customização das mensagens de erro de validação da tela de cadastro de usuário

--- app/Controllers/Qualiuser.php
+++ app/Controllers/Qualiuser.php
@@ -20,15 +20,36 @@ class Qualiuser extends BaseController
 		// Função que insere um novo usuário
 		public function register() {
 				if($this->request->getMethod() == 'post') {
-						// Define as regras de validações dos campos de usuário
-						$rules = [
+						// Valida os campos de usuário 
+						if(!$this->validate([
 								'username'      => 'required|min_length[3]|max_length[50]',
 								'password'      => 'required|min_length[6]|max_length[255]',
 								'confpassword'  => 'required|matches[password]',
 								'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]'
-						];
-
-						if(!$this->validate($rules)) {
+						],
+						[   // Errors
+								'username' => [
+										'required' => 'Preencha o nome do usuário',
+										'min_length' => 'O nome do usuário deve conter pelo menos 3 caracteres.',
+										'max_length' => 'O nome do usuário não pode ter mais de 50 caracteres.'
+								],
+								'password' => [
+										'required' => 'Preencha a senha',
+										'min_length' => 'A senha deve conter pelo menos 6 caracteres.',
+										'max_length' => 'A senha não pode ter mais de 50 caracteres.'
+								],
+								'confpassword' => [
+										'required' => 'Preencha a confirmação da senha',
+										'matches' => 'A senha digitada não é igual a confirmação.'
+								],
+								'email' => [
+										'required' => 'Preencha seu e-mail',
+										'min_length' => 'O e-mail deve conter pelo menos 6 caracteres.',
+										'max_length' => 'O e-mail não pode ter mais de 50 caracteres.',
+										'valid_email' => 'Digite um endereço de e-mail válido.',
+										'is_unique' => 'E-mail existente.'
+								]
+						])) {
 								$data['validation'] = $this->validator;
 								$this->index($data);
 						}
