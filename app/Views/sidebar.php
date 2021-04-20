fa1857d5ff2724a5de5515a9620e5215c63898c7
snowneves <neves1610@gmail.com>
2021-04-20 17:14:26 -0300

    Menu atualizado

--- app/Views/sidebar.php
+++ app/Views/sidebar.php
@@ -98,16 +98,10 @@
             <!-- Nav Item - Charts -->
             <li class="nav-item">
                 <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+                    <i class="fas fa-fw fa-network-wired"></i>
+                    <span>API's</span></a>
             </li>
 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
