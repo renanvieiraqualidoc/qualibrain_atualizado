466a3bd87a0ed6cb3821921a900ff3b81c918c4e
Renan Brito <renan.brito@qualidoc.com.br>
2021-05-18 09:58:51 -0300

    Correção das margens do gráfico de linha

--- app/Models/SalesModel.php
+++ app/Models/SalesModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 class SalesModel extends Model{
     public function getSalesByDate($initial_date, $final_date, $department) {
         $query = $this->db->table('vendas')
-                          ->select('DATE_FORMAT(data, "%Y-%m") as data, faturamento, round((faturamento - price_cost * qtd), 2) as margin')
+                          ->select('DATE_FORMAT(data, "%Y-%m") as data, faturamento, round((faturamento - price_cost), 2) as margin')
                           ->where('data >=', $initial_date)
                           ->where('data <=', $final_date)
                           ->orderBy('data asc');
